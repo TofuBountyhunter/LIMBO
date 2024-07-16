77f82290e8dd74b74cc31e00ad9368a1299a57a8
Tofu <tofu4898@gmail.com>
2024-07-16 17:33:40 +0530

    Start

---
+++ LIMBO/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='w-full h-screen bg-slate-800 flex justify-center items-center '>
+      <div className='text-5xl text-white'>This is Tofu!</div>
+    </div>
+    </>
+  )
+}
+
+export default App
